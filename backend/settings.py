930b908c12e63829e9e321b53cb3686103ca4bd5
Jesse <jesseweijapan@gmail.com>
2021-03-08 07:14:47 +0900

    Config for heroku

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,5 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
